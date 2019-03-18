57ed9db222fc9032dfdf18b2719bd6f2680a8211
lieby weintraub <liebahinda@gmail.com>
2019-03-18 14:55:25 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,17 @@
   class Song
  
   attr_accessor :title, :artist
+  
+  def initialize 
+    title=@title
+    artist=@artist 
+  end
+end
  
   
-def artist=(artist)
-    if artist.is_a? Artist
-      @artist = artist
-    else
-      nil 
-    end 
-  end
+#def artist=(artist)
+   #  artist.is_a? Artist
+   #   @artist = artist
+  #  end 
+ # end
  
-end
